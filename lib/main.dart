bf00e44763be94a1cb20f34d46292be33968ec37
Abhinav <aabhikumar2003@gmail.com>
2023-06-24 08:42:06 +0530

    Minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_manager/firebase_options.dart';
 import 'package:expense_manager/screens/sign_in_screen.dart';
+import 'package:expense_manager/widgets/bottom_nav_bar.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -10,28 +11,17 @@ Future<void> main() async {
   );
   runApp(const MyApp());
 }
-
+int index_color=0;
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const SignIn(),
+      home: SignIn(),
     );
   }
 }
